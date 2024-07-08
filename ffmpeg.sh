c1fe6fa0601705cdb1ced7ec80c46c59f911e593
pudja.gemilang <pudjagemilang2001@gmail.com>
2024-07-08 11:22:52 +0700

    modify docs

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i '.\data\update_testing1.mp4' -c:v libx264 -c:a aac -preset slower -tune zerolatency -f flv rtmp://localhost:1935/live
+ffmpeg -re -i '.\input\update_testing1.mp4' -c:v libx264 -c:a aac -preset slower -tune zerolatency -f flv rtmp://localhost:1935/live
